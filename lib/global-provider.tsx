5457d109ef0f2d2605ded206cc7b2f78de246928
John-Dembaremba <johndembaremba97@gmail.com>
2025-01-11 02:27:16 +0200

    added(AppAuth): added layout handling to the app auth

--- lib/global-provider.tsx
+++ lib/global-provider.tsx
@@ -34,7 +34,6 @@ export const GlobalProvider = ({ children }: GlobalProviderProps) => {
   });
 
   const isLoggedIn = !!user;
-  console.log(JSON.stringify(user));
   return (
     <GlobalContext.Provider
       value={{
