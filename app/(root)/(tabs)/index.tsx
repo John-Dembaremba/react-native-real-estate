9d6074c26cfa36075e5b0270e2f4cd592109bdf9
John-Dembaremba <johndembaremba97@gmail.com>
2025-01-12 23:16:57 +0200

    added(HomePage): added featured and our recommendation with cards

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,7 +1,8 @@
+import { Card, FeaturedCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -24,6 +25,48 @@ export default function Index() {
         </View>
 
         <Search />
+
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text
+                className="text-base font-rubik-bold
+               text-primary-300"
+              >
+                See all
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View className="flex flex-row gap-5 mt-5">
+          <FeaturedCard />
+          <FeaturedCard />
+        </View>
+
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Our Recommendation
+            </Text>
+            <TouchableOpacity>
+              <Text
+                className="text-base font-rubik-bold
+               text-primary-300"
+              >
+                See all
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="flex flex-row mt-5 gap-5">
+            <Card />
+            <Card />
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
